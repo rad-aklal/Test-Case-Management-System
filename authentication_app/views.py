96beb53a628ec9ddd8d6d495df050dfeea0e003e
Angel Dhakal <anjaldhakal01@gmail.com>
2020-07-13 00:50:41 +0545

    login,register renders respective pages

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse("Hello World")
+    return redirect('register/')
+
+
+def register(request):
+    return render(request,'authentication_app/register.html')
+
+
+def login(request):
+    return render(request,'authentication_app/login.html')
+
