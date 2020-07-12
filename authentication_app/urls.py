25ddaa18e6ed9a49baf2287b037641f3e011bc63
Angel Dhakal <anjaldhakal01@gmail.com>
2020-07-13 00:53:10 +0545

    login,register path set

--- authentication_app/urls.py
+++ authentication_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import home
+from . views import login,home,register
 
 urlpatterns =[
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('login/',login,name='login'),
+    path('register/',register,name='register'),
 ]
