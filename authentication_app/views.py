1fd2137db199e01d6581e46a1ca3d11a967ebc11
Angel Dhakal <anjaldhakal01@gmail.com>
2020-07-14 17:14:48 +0545

    register functionality added

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 
 def home(request):
-    return redirect('register/')
+    return render(request,"case_form.html")
 
 
 def register(request):
-    return render(request,'authentication_app/register.html')
+    if request.method == "POST":
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password_field']
+        password2=request.POST['confirm_password']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username is already taken")
+                return redirect('/')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email already taken")
+                return redirect('/')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password1)
+                user.save();
+        else:
+            messages.info(request,"Password doesn't match")
+            return redirect('/')
+        return redirect('/login')
+    else:
+        return render(request,'authentication_app/register.html')
 
 
 def login(request):
-    return render(request,'authentication_app/login.html')
+    if request.method=="POST":
+        username
+    else:
+        return render(request,'authentication_app/login.html')
 
